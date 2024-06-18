ddcca9d95f0108c7b9bd5c4a6cf40ccefcac274c
lucas <manlucas603@proton.me>
2024-06-18 17:20:17 +0300

    changes: removed text shadow (looked goofy) added sigma logo (looks alright for now) added sigma's title background when disabling shader

--- src/routes/hud/elements/Watermark.svelte
+++ src/routes/hud/elements/Watermark.svelte
@@ -1,10 +1,25 @@
-<img class="watermark" src="img/lb-logo.svg" alt="watermark" />
+<span class="line1">Sigma</span>
+<span class="line2">Jello</span>
 
 <style lang="scss">
-    .watermark {
-        //position: fixed;
-        //top: 15px;
-        //left: 15px;
-        width: 165px;
+
+    @import "../../../colors.scss";
+
+    .line1 {
+        font-weight: 100;
+        color: rgba(white, 0.5);
+        font-size: 50px;
+        //background-color: black;
+        //text-shadow: 0px 0px 20px rgba(black, 1);
+    }
+
+    .line2 {
+        font-weight: 100;
+        color: rgba(white, 0.5);
+        font-size: 25px;
+        //background-color: black;
+        align-self: flex-start;
+        padding-right: 1915px;
+        //text-shadow: 0px 0px 20px rgba(black, 1);
     }
 </style>
