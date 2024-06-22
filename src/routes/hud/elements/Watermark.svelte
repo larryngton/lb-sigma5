1a94cf88e132cf3d7692279568cc06646356f5f0
lucas <manlucas603@proton.me>
2024-06-22 23:59:48 +0300

    sigma logo

--- src/routes/hud/elements/Watermark.svelte
+++ src/routes/hud/elements/Watermark.svelte
@@ -1,25 +1,11 @@
-<span class="line1">Sigma</span>
-<br>
-<span class="line2">Jello</span>
+<img class="watermark" src="img/sigmalogo.png" alt="watermark" />
 
 <style lang="scss">
-
-
     @import "../../../colors.scss";
-
-    .line1 {
-        font-weight: 100;
-        color: rgba(white, 0.5);
-        font-size: 50px;
-        //text-shadow: 0px 0px 20px rgba(black, 0.7);
-    }
-
-    .line2 {
-        font-weight: 100;
-        color: rgba(white, 0.5);
-        font-size: 20px;
-        bottom: 5px;
-        align-self: flex-start;
-        //text-shadow: 0px 0px 20px rgba(black, 1);
+    .watermark {
+        //position: fixed;
+        //top: 15px;
+        //left: 15px;
+        width: 200px;
     }
 </style>
